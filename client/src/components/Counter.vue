af42429532935826a7315fba9d469c02e0eec043
Leon Blakey <code@leonblakey.com>
2021-08-21 18:56:19 -0400

    re-org, split client and server

---
+++ client/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <code>we are {{ counter }}</code>
+  <button v-on:click="inc">clicker</button>
+</template>
+
+<script lang="ts">
+  import {getElement} from  "../../../../ytsubs/src/utils/xmlUtils"
+
+  import { ref, defineComponent } from 'vue'
+  export default defineComponent({
+    name: "mycounter",
+    data: () => {
+      return {
+        counter: 0
+      }
+    },
+    // props: {
+    //   counter: 0,
+    // },
+    methods: {
+      inc() {
+        this.counter++
+      }
+    }
+  })
+</script>
+
+<style scoped>
+code {
+  background-color: blue;
+}
+</style>
