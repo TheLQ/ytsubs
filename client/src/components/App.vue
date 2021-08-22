101aaf30d0d60d28427ccab3f31f4f47981d1bf2
Leon Blakey <code@leonblakey.com>
2021-08-22 14:58:35 -0400

    client: switch to vue-router

---
+++ client/src/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Hello App!</h1>
+  <p>
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <div id="content">
+    <router-view></router-view>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Videos from './Videos.vue'
+import Counter from './Counter.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Videos,
+    Counter
+  }
+})
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+
+#content {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
+import Videos from './components/Videos.vue'
+import Counter from './components/Counter.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: Videos },
+    { path: '/about', component: Counter },
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
